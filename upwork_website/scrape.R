068f41bf002df57069016ec62924d7ec4aefb341
Michael Topper <miketopper123@gmail.com>
2022-04-05 11:39:38 -0700

    sample doc

---
+++ upwork_website/scrape.R
@@ -0,0 +1,19 @@
+## Purpose of script: scrape this website
+# ## need: 
+# Data needed (per column)
+# 1.  First Name
+# 2. Last Name
+# 3. Company Name
+# 4. Title (if available, example: Two Small Fish Ventures  Analyst (Title is Analyst)
+#           5. Investment Range
+#           6. Sweet Spot
+#           7. Website
+#           8.  City, State, Country
+## Author: Michael Topper
+##
+## Date Last Edited: 2021-11-07
+##
+
+library(tidyverse)
+
+link <- "https://signal.nfx.com/investor-lists/top-who-invested-in-female-founders-investors"
